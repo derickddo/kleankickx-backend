9720091ba23defeaa3ca766d042799593cd531a9
derickddo <dansoderrick80@yahoo.com>
2025-06-17 14:58:57 +0000

    Add AJAX form handling and prettify.js for code formatting
    - Implemented AJAX form submission in load-ajax-form.js to enhance user experience.
    - Added prettify-min.js for improved code syntax highlighting and formatting in the application.

--- settings/base.py
+++ settings/base.py
@@ -91,7 +91,7 @@ CORS_ALLOW_CREDENTIALS = True  # Allow cookies to be sent with CORS requests
 
 
 MIDDLEWARE = [
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Middleware for serving static files in production
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # Middleware for CORS
@@ -251,6 +251,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
