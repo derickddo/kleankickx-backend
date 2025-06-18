e352170b8df996a9a973112a36aae69f3dff1471
derickddo <dansoderrick80@yahoo.com>
2025-06-18 10:08:27 +0000

    Update ALLOWED_HOSTS setting to allow all hosts in development

--- settings/base.py
+++ settings/base.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else [ 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 
 # Application definition
