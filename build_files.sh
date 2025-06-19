cc590e694855fa44c3f197f10ef7a69adb959324
derickddo <dansoderrick80@yahoo.com>
2025-06-19 10:45:24 +0000

    Remove Django static and media files from .gitignore; update build script to ensure static files are collected during build

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "BUILD START"
-python3.9 -m pip install --no-cache-dir -r requirements.txt
+set -e
+echo "🔧 Collecting static files..."
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
